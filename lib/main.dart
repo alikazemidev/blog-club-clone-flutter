7f55ebc1b8f4c5a15d7f31bac08c1205351f0eb7
alikz <ali6316k@gmail.com>
2022-11-06 19:30:43 +0330

    complete bottom nav bar first screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:blog_club7/data.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -29,6 +38,12 @@ class MyApp extends StatelessWidget {
           ),
         ),
         textTheme: TextTheme(
+          caption: TextStyle(
+            fontFamily: defFontFamily,
+            fontWeight: FontWeight.w700,
+            fontSize: 10,
+            color: Color(0xff7b8bb2),
+          ),
           headline6: TextStyle(
             fontSize: 18,
             fontFamily: defFontFamily,
@@ -67,7 +82,17 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Stack(
+        children: [
+          Positioned.fill(child: HomePage()),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: BottomNavigation(),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -503,6 +528,107 @@ class PostListItem extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class BottomNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 85,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 65,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 20,
+                    color: Color(0xff9b8487).withOpacity(.3),
+                  ),
+                ],
+                color: Colors.white,
+              ),
+              //  Bottom nav Item
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  BottomNavigationItem(
+                    icon: 'Home',
+                    activeIcon: 'Home',
+                    title: 'Home',
+                  ),
+                  BottomNavigationItem(
+                    icon: 'Articles',
+                    activeIcon: 'Articles',
+                    title: 'Articles',
+                  ),
+                  SizedBox(width: 8),
+                  BottomNavigationItem(
+                    icon: 'Search',
+                    activeIcon: 'Search',
+                    title: 'Search',
+                  ),
+                  BottomNavigationItem(
+                    icon: 'Menu',
+                    activeIcon: 'Menu',
+                    title: 'Menu',
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // plus button
+          Center(
+            child: Container(
+              height: 85,
+              width: 65,
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 65,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 4),
+                  color: firstGradientColor,
+                  borderRadius: BorderRadius.circular(32.5),
+                ),
+                child: Image.asset('assets/img/icons/plus.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavigationItem extends StatelessWidget {
+  final String? icon, title, activeIcon;
+  const BottomNavigationItem({
+    super.key,
+    this.icon,
+    this.title,
+    this.activeIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/img/icons/$icon.png'),
+        SizedBox(
+          height: 4,
+        ),
+        Text(
+          title!,
+          style: Theme.of(context).textTheme.caption,
+        ),
+      ],
     );
   }
 }
